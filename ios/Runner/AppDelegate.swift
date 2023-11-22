e044ab3e3ab28d24666b84345b37adac2b84fdda
Tom Citriniti <Thomas.Citriniti@ge.com>
2023-11-21 20:05:10 -0500

    Update to latest flutter and pubs an fixes for ios.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
+    
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyDaPv6_DyIkij4lfO7Blc8VxjqY3g7iASE")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xc_ski_tracker2/main.dart';
+import 'package:xc_ski_tracker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
